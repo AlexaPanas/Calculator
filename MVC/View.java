c87176d111949739c8ab8928cf1408b13a65fcb9
Alexa <AlexaPanashenko@proton.me>
2025-02-20 19:34:07 +0200

    Create View.java
    MVC - View created

---
+++ MVC/View.java
@@ -0,0 +1,34 @@
+package MVC;
+
+import java.util.Scanner;
+
+public class View {
+
+     Scanner scanner = new Scanner(System.in);
+
+    private String getStr(String str){
+        System.out.println(str);
+        return scanner.next();
+    }
+    public double getValue(String str){
+        System.out.println(str);
+        return  scanner.nextDouble();
+
+    }
+    public void printResult(Double result){
+        System.out.println("Result: " + result);
+
+    }
+    public void print_msg(String msg){
+        System.out.println(msg);
+    }
+    public void print_Menu(){
+        System.out.println("------ CHOOSE OPERATION ------");
+        System.out.println("1. + ");
+        System.out.println("2. - ");
+        System.out.println("3. * ");
+        System.out.println("4. / ");
+        System.out.println("5. exit");
+
+    }
+}
